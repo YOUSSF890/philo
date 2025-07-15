30ad32c9f37c04eafe86bdbf9ad31561e586cd87
Youssef Lagzouli <ylagzoul@e3r9p17.1337.ma>
2025-07-15 21:49:29 +0100

    hdgj

--- philo_bonus/utils_philo2_bonus.c
+++ philo_bonus/utils_philo2_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ylagzoul <ylagzoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 16:25:06 by ylagzoul          #+#    #+#             */
-/*   Updated: 2025/07/15 20:06:02 by ylagzoul         ###   ########.fr       */
+/*   Updated: 2025/07/15 21:48:58 by ylagzoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	chick_deid(t_data *data)
 {
+	int i;
+
 	if (ft_tim_dil() - data->last_eat >= data->time_to_diel
 		&& data->last_eat != 0)
 	{
@@ -21,7 +23,12 @@ void	chick_deid(t_data *data)
 		sem_post(data->forks);
 		sem_wait(data->print);
 		printf("%ld %d is died\n", ft_tim_dil() - data->one_tim, data->id);
-		sem_post(data->died);
+		i = 0;
+		while(i < data->nbr_of_philo)
+		{
+			sem_post(data->died);
+			i++;
+		}
 		exit(1);
 	}
 }
